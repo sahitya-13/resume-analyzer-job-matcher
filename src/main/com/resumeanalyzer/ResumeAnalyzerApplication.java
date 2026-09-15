349f1cda67e677f33e842cf1f00658fd84ba1ba9
sahitya-13 <sahityae123@gmail.com>
2026-09-15 12:03:26 +0530

    Add main Java class

---
+++ src/main/com/resumeanalyzer/ResumeAnalyzerApplication.java
@@ -0,0 +1,9 @@
+package com.resumeanalyzer;
+
+public class ResumeAnalyzerApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Resume Analyzer and Job Matcher");
+        System.out.println("Java project started successfully.");
+    }
+}
